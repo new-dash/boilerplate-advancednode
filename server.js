bb17305d768f07a9391f54f69434a2223cb672ac
new-dash <jacob.kocina@pm.me>
2022-04-11 16:21:30 -0500

    framework for user serializations for sessions

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const myDB = require('./connection');
+const ObjectID = require('mongodb').ObjectID;
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 
@@ -24,6 +25,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.set('view engine', 'pug');
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+    myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, null);
+    });
+});
 
 app.route('/').get((req, res) => {
     res.render('pug', {
