82e197994c6650cff6cfc72830e6a226e0fd0d5d
new-dash <jacob.kocina@pm.me>
2022-04-11 13:28:49 -0500

    Setting the views directory

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
-    res.render('/pug');
+    res.render('pug');
 });
 
 const PORT = process.env.PORT || 3000;
