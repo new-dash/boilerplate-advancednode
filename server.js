217d88015394b3153e4a1c710d37ed9a6093d928
new-dash <jacob.kocina@pm.me>
2022-04-11 15:43:27 -0500

    adding user registration and session handling

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+
 
 const app = express();
 
@@ -10,6 +14,15 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
