2dce9c839bdd0d5c8cea382f61a8e2ff97e64227
new-dash <jacob.kocina@pm.me>
2022-04-11 16:58:03 -0500

    connecting db and updating local vars

--- server.js
+++ server.js
@@ -26,13 +26,33 @@ app.use(passport.session());
 
 app.set('view engine', 'pug');
 
-passport.serializeUser((user, done) => {
-    done(null, user._id);
-});
+myDB(async client => {
+    const myDataBase = await client.db('adv-node-db').collection('users');
 
-passport.deserializeUser((id, done) => {
-    myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(null, null);
+    // Be sure to change the title
+    app.route('/').get((req, res) => {
+        //Change the response to render the Pug template
+        res.render('pug', {
+            title: 'Connected to Database',
+            message: 'Please login'
+        });
+    });
+
+    // Serialization and deserialization here...
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+            done(null, doc);
+        });
+    });
+
+    // Be sure to add this...
+}).catch(e => {
+    app.route('/').get((req, res) => {
+        res.render('pug', { title: e, message: 'Unable to login' });
     });
 });
 
