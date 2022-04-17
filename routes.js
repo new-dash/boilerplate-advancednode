1801ec6e9ceb3da8f441548249f89545226b0f78
new-dash <jacob.kocina@pm.me>
2022-04-17 12:44:12 -0500

    Changing the github callback route back to get

--- routes.js
+++ routes.js
@@ -35,7 +35,7 @@ module.exports = function(app, myDataBase) {
     app.route('/auth/github').get(passport.authenticate('github'));
 
     app.route('/auth/github/callback')
-        .post(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+        .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
             res.redirect('/profile');
         });
 
