a946e4c3bd0b33e7eb413301d2006ef353b02b75
new-dash <jacob.kocina@pm.me>
2022-04-17 12:34:11 -0500

    Adding github strategy

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const GitHubStrategy = require('passport-github').Strategy;
 
 module.exports = function(app, myDataBase) {
 
