6ad844ef49ec5fa190bf4df12f93895a4a89a010
new-dash <jacob.kocina@pm.me>
2022-04-17 12:49:03 -0500

    setting the github auth in auth.js

--- auth.js
+++ auth.js
@@ -28,4 +28,16 @@ module.exports = function(app, myDataBase) {
             });
         }
     ));
+
+
+    passport.use(new GitHubStrategy({
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: "https://boilerplate-advanced-node.herokuapp.com/auth/github/callback"
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            console.log(profile);
+            //Database logic here with callback containing our user object
+        }
+    ));
 }
--- routes.js
+++ routes.js
@@ -4,17 +4,6 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const GitHubStrategy = require('passport-github').Strategy;
 
-passport.use(new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "https://boilerplate-advanced-node.herokuapp.com/auth/github/callback"
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        console.log(profile);
-        //Database logic here with callback containing our user object
-    }
-));
-
 module.exports = function(app, myDataBase) {
 
     // Be sure to change the title
