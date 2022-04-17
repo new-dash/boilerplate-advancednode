6d8ee3222f7fba0716cebcd57ec3e3f9ebf24a4e
new-dash <jacob.kocina@pm.me>
2022-04-17 12:38:59 -0500

    Changing callback from get to post

--- routes.js
+++ routes.js
@@ -35,7 +35,7 @@ module.exports = function(app, myDataBase) {
     app.route('/auth/github').get(passport.authenticate('github'));
 
     app.route('/auth/github/callback')
-        .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+        .post(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
             res.redirect('/profile');
         });
 
