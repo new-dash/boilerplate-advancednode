7faab1186a09ef5f91c139be2a01cef04dc64cc4
new-dash <jacob.kocina@pm.me>
2022-04-23 09:17:52 -0500

    Sockets enabled

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ require('dotenv').config();
 
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
+const auth = require('./auth.js');
+const routes = require('./routes.js');
+
 const express = require('express');
-const session = require('express-session');
 const myDB = require('./connection');
 const ObjectID = require('mongodb').ObjectID;
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-
 const bcrypt = require('bcrypt');
-const routes = require('./routes.js');
-const auth = require('./auth.js');
+const session = require('express-session');
+
 
 const app = express();
-app.set('view engine', 'pug');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
+app.set('view engine', 'pug');
 fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -48,6 +52,10 @@ myDB(async(client) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+io.on('connection', socket => {
+    console.log('A user has connected');
+});
+
+http.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port ' + process.env.PORT);
 });
