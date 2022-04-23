f8ba4f788a6a5ef054d903f63dd7bc8d3900fc3a
new-dash <jacob.kocina@pm.me>
2022-04-23 10:28:34 -0500

    Moving socket code to client.js

--- public/client.js
+++ public/client.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
     /*global io*/
     let socket = io();
 
+    io.on('connection', socket => {
+        console.log('A user has connected');
+    });
+
+
     // Form submittion with new message in field with id 'm'
     $('form').submit(function() {
         var messageToSend = $('#m').val();
